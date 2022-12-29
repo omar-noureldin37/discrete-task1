49ec4b6ca8e4eb783e299a0d9ba20feeee3ac912
omar-noureldin37 <121605096+omar-noureldin37@users.noreply.github.com>
2022-12-29 10:34:39 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prime_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Start number: ");
+            int s = int.Parse(Console.ReadLine());
+
+            Console.Write("End numder: ");
+            int e = int.Parse(Console.ReadLine());
+
+            bool prime = true;
+
+            for (int x = s; x <= e; x++)
+            {
+                for (int y = 2; y <= x; y++)
+                {
+                    if (x % y == 0 && x != y)
+                    {
+                        prime = false;
+                        break;
+                    }
+                }
+                if (prime && x > 1)
+                {
+                    Console.WriteLine(x);
+                }
+                prime = true;
+            }
+            Console.WriteLine("_______________");
+
+        }
+    }
+}
